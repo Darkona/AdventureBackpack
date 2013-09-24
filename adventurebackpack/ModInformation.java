531ee880a9f5fad3dc15e2fbfe1ebdf611426213
Darkona <javier.darkona@gmail.com>
2013-09-24 00:08:46 -0430

    Fixed version

--- adventurebackpack/ModInformation.java
+++ adventurebackpack/ModInformation.java
@@ -7,5 +7,5 @@ public class ModInformation {
 	public static final String VERSION = "a1.0";
 	public static final String CHANNEL = "advbackpacklol";
 	public static final String AUTHOR = "Darkona";
-	public static final String MCVERSION = "1.6.2 - 1.6.4";
+	public static final String MCVERSION = "1.6.4";
 }
